a7ff7c556f8527bcfbd461aac962d2cd374dac7a
mufidzfaizin <muhammad_alfaizin_23rpl@student.smktelkom-mlg.sch.id>
2016-11-07 10:05:36 +0700

    RecyclerView List

---
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/recyclerview1/model/Hotel.java
@@ -0,0 +1,22 @@
+package id.sch.smktelkom_mlg.learn.recyclerview1.model;
+
+import android.graphics.drawable.Drawable;
+
+/**
+ * Created by mufid on 07/11/2016.
+ */
+public class Hotel
+{
+
+        public String judul;
+        public String deskripsi;
+        public Drawable foto;
+
+        public Hotel(String judul, String deskripsi, Drawable foto)
+        {
+            this.judul = judul;
+            this.deskripsi = deskripsi;
+            this.foto = foto;
+        }
+
+}
